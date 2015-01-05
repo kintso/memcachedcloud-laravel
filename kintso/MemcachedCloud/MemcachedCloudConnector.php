31667d9c8df77e95169419af0f219dc620f862f7
kintso <julien.herbin@yahoo.fr>
2015-01-05 10:52:58 +0100

    Initial commit

---
+++ kintso/MemcachedCloud/MemcachedCloudConnector.php
@@ -0,0 +1,44 @@
+<?php 
+
+namespace Pozpom\MemcachedCloud;
+use Memcached;
+
+class MemcachedCloudConnector {
+
+    /**
+     * Extend the Memcached connection to use MemcachedCloud via Heroku
+     *
+     * @param array $servers
+     * @throws \RuntimeException
+     * @return \Memcached
+     */
+    public function connect(array $servers)
+    {
+        $memcached = $this->getMemcached();
+            
+        // Set Elasticache options here
+        if (defined('\Memcached::OPT_BINARY_PROTOCOL')) {
+            $memcached->setOption(Memcached::OPT_BINARY_PROTOCOL, true);
+        }
+
+        $memcached->addServers(array_map(function($server) { return explode(':', $server, 2); }, explode(',', $_ENV['MEMCACHEDCLOUD_SERVERS'])));
+        $memcached->setSaslAuthData($_ENV['MEMCACHEDCLOUD_USERNAME'], $_ENV['MEMCACHEDCLOUD_PASSWORD']);
+
+        if ($memcached->getVersion() === false) {
+            throw new \RuntimeException("Could not establish Memcached connection.");
+        }
+
+        return $memcached;
+    }
+
+    /**
+     * Get a new Memcached instance.
+     *
+     * @return \Memcached
+     */
+    protected function getMemcached()
+    {
+        return new Memcached;
+    }
+
+}
---
+++ kintso/MemcachedCloud/MemcachedCloudServiceProvider.php
@@ -0,0 +1,47 @@
+<?php 
+
+namespace kintso\MemcachedCloud;
+use Illuminate\Support\ServiceProvider;
+
+// Based on https://github.com/atyagi/elasticache-laravel
+
+class MemcachedCloudServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+            $servers = $this->app['config']->get('cache.memcached');
+            $memcachedCloud = new MemcachedCloudConnector();
+            $memcached = $memcachedCloud->connect($servers);
+
+            $this->app->getProviderRepository()->load($this->app, array('Illuminate\Cache\CacheServiceProvider'));
+
+            $this->app->make('cache')->extend('memcached', function() use ($memcached) {
+                /** @noinspection PhpUndefinedNamespaceInspection */
+                /** @noinspection PhpUndefinedClassInspection */
+                return new \Illuminate\Cache\Repository(
+                    new \Illuminate\Cache\MemcachedStore($memcached, $this->app['config']->get('cache.prefix')));
+            });
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array();
+	}
+}
